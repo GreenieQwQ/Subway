4ec1dc622fa82853406498168e4d3cdf9754194c
Greenie <1356672774@qq.com>
2019-12-24 00:44:00 +0800

    fix bugs

--- Graph.h
+++ Graph.h
@@ -44,23 +44,10 @@ public:
     static map<string, pair<string, string>> routeEnd; //线路， <起点，终点>
 public:
     Graph() {}
-<<<<<<< HEAD
-    void addAdjecnt(const string& nameA, const string& nameB,  weight w, const string& route, State s); 
-    /**
-     * 增加邻边 若没有点a/b则创建点a/b
-     * s = End 代表A->B方向为终点站方向
-     */
-    void eraseVertex(const string& name); //删除名字为name的站点
-    size_t print_path(const string& source, const string& destination);
-     /**
-      * 打印实际最短路径，格式：(起点,src) -> (线路,中间站点) -> (线路, dst) (坐哪条线路, 到哪个站)  返回路径的长度
-      */
-=======
     void eraseVertex(const string& name); //删除名字为name的站点
     void addAdjecnt(const string& nameA, const string& nameB,  weight w, const string& route); //增加邻边 若没有点a/b则创建点a/b
     size_t print_path(const string& source, const string& destination); //打印路径 (src,v1) -> (1,V4) -> (1, v6) (哪条线路, 到哪个站)
     //void dijkstra(const string& source, const string& destination);    
->>>>>>> b6399cd1465f310764b1bcaa6cbcb6e784c2f8dd
 private:
     int number_of_vertex;
     map<string, Vertex> vertex_list;
